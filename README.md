5d0e62ddc3f5408a1de46274a4eae147bce22c0f
Vinicius Viana <viniciusviana020@gmail.com>
2024-08-13 13:23:34 -0300

    feat: add uspi driver for keyboard

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # Low Level Tetris
 A Tetris game coded in C, running directly on bare metal Raspberry Pi 2. No OS, direct hardware control for graphics and input.
 
+## USPi
+
+USPi is an USB driver for Raspberry Pi written in C. Here is the original repository: [USPi](https://github.com/rsta2/uspi)
+
+USPi is used with little adaptations in our Low Level Tetris project. The needed files are inside the `uspi` directory.
+
 ## Compile Tests
 
 ```bash
 g++ -o tests/tetris tests/tetris.c -I/usr/local/include/SDL2 -L/usr/local/lib -lSDL2
 ```
 
+## Generate .img file
 
+The USPi driver is adapted here to work by default for the Raspberry Pi 2. For that, just simply use the `makeall` script
+
+```bash
+./makeall
+```
+
+It is also possible to use it for the Raspberry Pi 3 (32 bit architecture):
+
+```bash
+./makeall RASPPI=3 
+```
+
+To remove all the generated files:
+```bash
+./makeall clean
+```
--- main.c
+++ main.c
@@ -1,78 +1,54 @@
+//
+// main.c
+//
+#include <uspienv.h>
+#include <uspi.h>
+#include <uspios.h>
+#include <uspienv/util.h>
 
-#include <stdio.h>
-#include "fb.h"
-#include "cpu.h"
+static const char FromSample[] = "sample";
 
-extern void delay(int mili);
-
-const int BLOCK_SIZE = 16;
-
-const int frame_width = 10 * BLOCK_SIZE;
-const int frame_height = 20 * BLOCK_SIZE;
-const int frame_thickness = 4;
-
-const int frame_pos_x = WIDTH / 2 - frame_width / 2 - frame_thickness;
-const int frame_pos_y = HEIGHT / 2 - frame_height / 2 - frame_thickness;
-
-void draw_line(int x, int y, int length, unsigned color, int thickness, bool vertical)
+static void KeyPressedHandler (const char *pString)
 {
-   for (int i = 0; i < thickness; i++)
-      for (int j = 0; j < length; j++)
-         fb_pixel(x + (!vertical ? j : 0), y + (vertical ? j : 0), color);
+	ScreenDeviceWrite (USPiEnvGetScreen (), pString, strlen (pString));
 }
 
-void draw_rect(int x, int y, int width, int height, int thickness, unsigned color) {
-   draw_line(x, y, height + 2 * thickness, color, thickness, true);
-   draw_line(x + width + 2 * thickness, y, height + 2 * thickness, color, thickness, true);
-   draw_line(x, y, width + 2 * thickness, color, thickness, false);
-   draw_line(x, y + height + 2 * thickness, width + 2 * thickness, color, thickness, false);
-}
-
-void draw_frame()
+int main (void)
 {
-   unsigned white = pack_color(255, 255, 255);
-   draw_rect(frame_pos_x, frame_pos_y, frame_width, frame_height, frame_thickness, white);
-}
+	if (!USPiEnvInitialize ())
+	{
+		return EXIT_HALT;
+	}
+	
+	if (!USPiInitialize ())
+	{
+		LogWrite (FromSample, LOG_ERROR, "Cannot initialize USPi");
 
-void draw_square(int pos_x, int pos_y, unsigned color)
-{
-   for (int i = 0; i < BLOCK_SIZE; i++)
-      for (int j = 0; j < BLOCK_SIZE; j++)
-         fb_pixel(pos_x + i, pos_y + j, color);
-}
+		USPiEnvClose ();
 
-void draw_block(int pos_x, int pos_y, unsigned color)
-{
-   draw_square(pos_x, pos_y, color);
-   draw_square(pos_x - BLOCK_SIZE, pos_y + BLOCK_SIZE, color);
-   draw_square(pos_x, pos_y + BLOCK_SIZE, color);
-   draw_square(pos_x + BLOCK_SIZE, pos_y + BLOCK_SIZE, color);
-}
+		return EXIT_HALT;
+	}
+	
+	if (!USPiKeyboardAvailable ())
+	{
+		LogWrite (FromSample, LOG_ERROR, "Keyboard not found");
 
-void main(void)
-{
-   enable_cache();
-   bool ok = init_fb();
-   // seria sábio tratar o valor retornado por init_fb()...
+		USPiEnvClose ();
 
-   draw_frame();
+		return EXIT_HALT;
+	}
 
-   unsigned cyan = pack_color(0, 255, 255);
-   unsigned black = pack_color(0, 0, 0);
+	USPiKeyboardRegisterKeyPressedHandler (KeyPressedHandler);
 
-   int block_pos_x = frame_pos_x + frame_width / 2;
-   int block_pos_y = frame_pos_y;
+	LogWrite (FromSample, LOG_NOTICE, "Just type something!");
 
-   delay(10000000);
+	// just wait and turn the rotor
+	for (unsigned nCount = 0; 1; nCount++)
+	{
+		USPiKeyboardUpdateLEDs ();
 
-   for (;;)
-   {
-      draw_block(block_pos_x, block_pos_y, cyan);
-      delay(1000000);
-      if (block_pos_y + 2 * BLOCK_SIZE >= frame_pos_y + frame_thickness + frame_height)
-         for (;;)
-            ;
-      draw_block(block_pos_x, block_pos_y, black);
-      block_pos_y += BLOCK_SIZE;
-   }
+		ScreenDeviceRotor (USPiEnvGetScreen (), 0, nCount);
+	}
+
+	return EXIT_HALT;
 }
